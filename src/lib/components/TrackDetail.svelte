d65436d42f429abf71b54f9ed61a5dad1749e720
Max Sporer <maxsporer@Maxs-MacBook-Pro.local>
2023-12-25 00:33:27 -0500

    add challenge link to drawer

--- src/lib/components/TrackDetail.svelte
+++ src/lib/components/TrackDetail.svelte
@@ -2,12 +2,21 @@
   import type { DeezerTrack } from '$lib/types';
   import add_song from '$lib/svgs/add_song.svg';
   import Icon from '@iconify/svelte';
-  import { Button } from 'flowbite-svelte';
+  import { Button, Spinner } from 'flowbite-svelte';
   import PlayButton from './PlayButton.svelte';
   import { create } from '$lib/apiCalls';
+  import { page } from '$app/stores';
+  import { LinkSolid } from 'flowbite-svelte-icons';
+  import { showNotification } from '$lib/stores/SelectionStore';
   
   export let track: DeezerTrack;
   export let onDrawer: boolean = false;
+
+  let id: string = '';
+  let waiting: boolean = false;
+  let challengeLink: string;
+  
+  $: challengeLink = `${$page.url.origin}/guess?id=${id}`;
 </script>
 
 <div class='flex m-4 justify-between items-center'>
@@ -30,12 +39,41 @@
   {#if onDrawer}
     <div class='flex items-center justify-center gap-x-4'>
       <PlayButton src={track.preview} />
-      <Button
-        class='w-12 h-12 p-2.5 rounded-full focus-within:ring-0 dark:focus-within:ring-0'
-        on:click={() => create(track)}
-      >
-        <img src={add_song} alt=''/>
-      </Button>
+      {#if !waiting}
+        <Button
+          class='w-12 h-12 p-2.5 rounded-full focus-within:ring-0 dark:focus-within:ring-0'
+          on:click={async () => {
+            waiting = !waiting;
+            id = await create(track);
+            waiting = !waiting;
+          }}
+        >
+          <img src={add_song} alt=''/>
+        </Button>
+      {:else}
+        <Spinner />
+      {/if}
     </div>
   {/if}
 </div>
+{#if onDrawer && id}
+  <div class='flex items-center justify-between my-2 mx-4 p-2 rounded outline outline-2 outline-white'>
+    <div>
+      {challengeLink}
+    </div>
+    <div class='flex items-center gap-x-2 mr-2'>
+      <a href={challengeLink} class='hover:text-primary-700'>
+        <Icon icon='tabler:external-link' class='w-6 h-6'/>
+      </a>
+      <div class='hover:text-primary-500 '>
+        <LinkSolid
+          on:click={() => {
+            navigator.clipboard.writeText(challengeLink);
+            showNotification.set(true);
+          }}
+          class='outline-0'
+        />
+      </div>
+    </div>
+  </div>
+{/if}
