48958b7c742643de9582f2df5b308ded495ac334
Max Sporer <maxsporer@Maxs-MacBook-Pro.local>
2023-12-25 20:01:25 -0500

    add time displays to guess page

--- src/lib/components/PlayButton.svelte
+++ src/lib/components/PlayButton.svelte
@@ -3,21 +3,42 @@
   import { PlaySolid, PauseSolid } from 'flowbite-svelte-icons';
 
   export let src: string;
+  export let showTime: boolean = false;
+
   let audio: HTMLAudioElement;
   let isPlaying: boolean = false;
+  let currentTime: string;
+  let duration: string;
+
+  setInterval(() => {
+    if (audio && !duration) duration = new Date(audio.duration * 1000).toISOString().slice(15, 19);
+    if (audio) currentTime = new Date(audio.currentTime * 1000).toISOString().slice(15, 19);
+  }, 100);
 </script>
 
 <audio src={src} bind:this={audio} />
-<Button
+<div class='flex items-center justify-evenly'>
+  <div>
+    {#if showTime && currentTime}
+      {currentTime}
+    {/if}
+  </div>
+  <Button
   class='w-12 h-12 rounded-full focus-within:ring-0 dark:focus-within:ring-0'
   on:click={() => {
     isPlaying ? audio.pause() : audio.play();
     isPlaying = !isPlaying;
   }}
->
+  >
   {#if isPlaying}
-    <PauseSolid />
+  <PauseSolid />
   {:else}
-    <PlaySolid />
+  <PlaySolid />
   {/if}
 </Button>
+  <div>
+    {#if showTime && duration}
+      {duration}
+    {/if}
+  </div>
+</div>
