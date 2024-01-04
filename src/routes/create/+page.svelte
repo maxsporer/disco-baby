3e9f7ce70ca53aebd76fbd65d82dfa2ef245b45f
tsporer <tsporer@umich.edu>
2024-01-03 22:56:53 -0500

    add header with title and instructions

--- src/routes/create/+page.svelte
+++ src/routes/create/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
   import CreateChallengeDrawer from '$lib/components/CreateChallengeDrawer.svelte';
   import SearchBar from '$lib/components/SearchBar.svelte';
+  import Header from '$lib/components/Header.svelte';
 </script>
 
-<SearchBar placeholder='search song to create challenge' />
+<Header
+  title='How to Create a Challenge'
+  subtitle='Search for a song you want your friend to guess. Once you&#39ve found the song, click it, create the challenge, and send it to your friend!'
+/>
+<div class='h-[calc(100dvh-50px)] lg:h-[calc(100dvh-62px)]'>
+  <SearchBar placeholder='search song to create challenge' />
+</div>
 <CreateChallengeDrawer />
