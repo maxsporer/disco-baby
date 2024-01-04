5d1389a3749f75ab8e579e73a6e9820a3e77bc66
tsporer <tsporer@umich.edu>
2024-01-03 22:56:53 -0500

    add header with title and instructions

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,34 @@
+<script lang='ts'>
+  import { QuestionCircleSolid } from 'flowbite-svelte-icons'
+  import { Modal } from 'flowbite-svelte';
+
+  export let title: string;
+  export let subtitle: string;
+
+  let modelOpen = false;
+</script>
+
+<div class='text-center'>
+  <div class='flex justify-between items-center text-2xl lg:text-3xl lg:pt-4 border-b-2 border-b-gray-300 mx-4 pb-2 pt-2'>
+    <div>
+      disco-baby
+    </div>
+    <button
+      type='button'
+      on:click={() => (modelOpen = true)}
+    >
+      <QuestionCircleSolid class='w-6 h-6' />
+    </button>
+  </div>
+</div>
+<Modal
+  class='mt-10 md:w-[75%]'
+  placement={'top-center'}
+  title={title}
+  bind:open={modelOpen}
+  autoclose outsideclose
+>
+  <div class='-m-2'>
+    {subtitle}
+  </div>
+</Modal>
--- src/routes/guess/+page.svelte
+++ src/routes/guess/+page.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
   import Guess from '$lib/components/Guess.svelte';
+  import Header from '$lib/components/Header.svelte';
   import SearchBar from '$lib/components/SearchBar.svelte';
   import PlayButton from '$lib/components/track_actions/PlayButton.svelte';
   import { currentGuess, queuedGuess } from '$lib/stores/stores';
@@ -11,12 +12,16 @@
   setContext('page', 'guess');
 </script>
 
+<Header
+  title='How to Play'
+  subtitle='Listen to the song and search for what you think it is. Press your guess and then the arrow to submit your guess!'
+/>
 {#if data.Item}
   <div class='flex flex-col justify-between items-center'>
-    <div class='{$queuedGuess || $currentGuess ? 'h-[calc(100dvh-158px)]' : 'h-[calc(100dvh-90px)]'} w-full overflow-hidden'>
+    <div class='{$queuedGuess || $currentGuess ? 'h-[calc(100dvh-208px)] lg:h-[calc(100dvh-220px)]' : 'h-[calc(100dvh-140px)] lg:h-[calc(100dvh-152px)]'} w-full overflow-hidden'>
       <SearchBar placeholder='guess the song' />
     </div>
-    <Guess challengeTrack={data}/>
+    <Guess challengeTrack={data} />
     <div class='my-2 py-3 w-full border-t-2'>
       <PlayButton src={data.Item.preview.S} showTime={true} />
     </div>
