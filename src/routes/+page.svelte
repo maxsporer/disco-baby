ccc6aad466eb9957f7dc43c491665f88ca8bb6a5
Max Sporer <maxsporer@Maxs-MacBook-Pro.local>
2024-02-15 00:13:45 -0500

    fix visual bugs on mobile, selecting password input zooms in on mobile, blue outline of password input clips of enter button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,9 @@
 
 <form method='POST'>
 	<div class='flex flex-col {isSm ? 'mx-8' : 'mx-[30%]'} gap-y-2 mt-[160px] justify-center'>
-		<div class='flex'>
-			<Input class='rounded-none' type='password' name='passphrase' autocomplete='off' placeholder='password' />
-			<button class='bg-gray-50 p-2.5' type='submit'>
+		<div class='flex relative'>
+			<Input class='rounded-none pr-10' type='password' name='passphrase' autocomplete='off' placeholder='password' />
+			<button class='bg-gray-50 p-2 m-0.5 absolute right-0' type='submit'>
 				<ArrowRightSolid class='text-primary-600 focus:outline-0' />
 			</button>
 		</div>
