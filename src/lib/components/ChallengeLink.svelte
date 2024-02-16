6271752272e43766ee7a26d2921195bc5bac64c2
tsporer <tsporer@umich.edu>
2024-02-15 22:06:49 -0500

    WIP

--- src/lib/components/ChallengeLink.svelte
+++ src/lib/components/ChallengeLink.svelte
@@ -13,28 +13,31 @@
 </script>
 
 {#if $challengeId}
-  <div class='Challenge grid grid-flow-col grid-cols-2 mt-4 mb-2 mx-4 p-2 rounded outline outline-2 outline-white'>
+  <div class='Challenge grid grid-flow-col grid-cols-1 grid-rows-2 lg:grid-rows-1 mt-4 mb-2 mx-4 p-2 align-middle rounded outline outline-2 outline-white'>
     <div class='Challenge-link'>
-      {challengeLink}
+      <input type="text" id="first_name" class="bg-gray-100
+      text-gray-900 text-md select-auto rounded-md block w-full p-2.5" value={challengeLink}>
     </div>
-    <div class='flex items-center gap-x-2 ml-2 mr-1 pl-2 border-l-2'>
-      <a href={challengeLink} class='hover:text-primary-600'>
-        <Icon icon='tabler:external-link' class='w-6 h-6' />
-      </a>
-      <div class='hover:text-primary-600'>
+    <div class='flex lg:flex items-center items-stretch lg:ml-2 mt-[1.5px] lg:mt-1 lg:mt-0'>
+      <div class='lg:flex hover:border-slate-600 hover:bg-slate-600 lg:hover:bg-primary-800 lg:hover:border-primary-800 border-y-[12px] lg:border-[10px] border-x-[44px] lg:border-x-[50px] m-auto rounded-md bg-slate-500 border-slate-500 lg:bg-primary-700 lg:border-primary-700'>
+        <span class="hidden lg:flex mr-1">Copy</span>
+        <span class="hidden lg:flex mr-2">link</span>
         <LinkSolid
           on:click={() => {
             navigator.clipboard.writeText(challengeLink);
             open = true;
           }}
-          class='outline-0'
+          class='outline-0 w-6 h-6 lg:w-5 lg:h-5'
         />
       </div>
       {#if navigator.share}
-        <button class='hover:text-primary-600' on:click={() => {navigator.share({url: challengeLink})}}>
-          <Icon icon='tabler:share-2' class='h-6 w-6' />
+        <button class='hover:border-slate-600 hover:bg-slate-600 border-y-[8px] lg:border-[10px] border-x-[40px] lg:border-x-[50px] m-auto rounded-md bg-primary-700 border-primary-700' on:click={() => {navigator.share({url: challengeLink})}}>
+          <Icon icon='tabler:share-2' class='w-8 h-8' />
         </button>
       {/if}
+      <a href={challengeLink} class='hover:border-slate-600 hover:bg-slate-600 border-y-[8px] lg:border-[10px] border-x-[40px] lg:border-x-[50px] m-auto rounded-md bg-slate-500 border-slate-500'>
+        <Icon icon='tabler:external-link' class='w-8 h-8 lg:w-6 lg:h-6' />
+      </a>
     </div>
   </div>
   <Toast
